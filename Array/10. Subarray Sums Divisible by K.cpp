515bea7ee5c36c3d44f14c11ff34cba85e1e8bcf
ShreeshRD <93830912+ShreeshRD@users.noreply.github.com>
2024-07-18 00:27:32 +0530

    solved 10

--- Array/10. Subarray Sums Divisible by K.cpp
+++ Array/10. Subarray Sums Divisible by K.cpp
@@ -6,5 +6,18 @@ class Solution
 public:
     int subarraysDivByK(vector<int> &nums, int k)
     {
+        vector<int> map(k);
+        map[0] = 1;
+        int count = 0, sum = 0;
+        for (int num : nums)
+        {
+            sum += num;
+            sum %= k;
+            if (sum < 0)
+                sum += k;
+            count += map[sum];
+            map[sum]++;
+        }
+        return count;
     }
 };

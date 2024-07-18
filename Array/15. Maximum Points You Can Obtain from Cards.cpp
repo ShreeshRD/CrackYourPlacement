edcdb2fd7090655e3d0b84df58cfc39e1ab724a8
ShreeshRD <93830912+ShreeshRD@users.noreply.github.com>
2024-07-18 13:11:24 +0530

    solved 15

---
+++ Array/15. Maximum Points You Can Obtain from Cards.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxScore(vector<int> &cardPoints, int k)
+    {
+        int n = cardPoints.size(), lsum = 0, rsum = 0;
+        for (int i = 0; i < k; i++)
+            lsum += cardPoints[i];
+        int ans = lsum;
+        for (int i = 0; i < k; i++)
+        {
+            lsum -= cardPoints[k - 1 - i];
+            rsum += cardPoints[n - 1 - i];
+            ans = max(ans, lsum + rsum);
+        }
+        return ans;
+    }
+};

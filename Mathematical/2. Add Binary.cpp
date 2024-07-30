eb52ef2ccbcd22bec830571a9dbf6ebc180ef56f
ShreeshRD <93830912+ShreeshRD@users.noreply.github.com>
2024-07-30 16:21:35 +0530

    better_2

--- Mathematical/2. Add Binary.cpp
+++ Mathematical/2. Add Binary.cpp
@@ -6,39 +6,22 @@ class Solution
 public:
     string addBinary(string a, string b)
     {
-        string ans = "";
-        int na = a.size(), nb = b.size(), carry = 0;
-        for (int i = 0; i < max(na, nb); i++)
+        string ans;
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry)
         {
-            int a_val = 0, b_val = 0, val;
-            if (i < na)
-                a_val = a[na - 1 - i] - '0';
-            if (i < nb)
-                b_val = b[nb - 1 - i] - '0';
-            val = carry + a_val + b_val;
-            if (val == 0)
-            {
-                carry = 0;
-                ans = '0' + ans;
-            }
-            else if (val == 1)
-            {
-                carry = 0;
-                ans = '1' + ans;
-            }
-            else if (val == 2)
-            {
-                carry = 1;
-                ans = '0' + ans;
-            }
-            else
-            {
-                carry = 1;
-                ans = '1' + ans;
-            }
+            if (i >= 0)
+                carry += a[i--] - '0';
+            if (j >= 0)
+                carry += b[j--] - '0';
+            ans += carry % 2 + '0';
+            carry /= 2;
         }
-        if (carry)
-            ans = '1' + ans;
+
+        reverse(begin(ans), end(ans));
         return ans;
     }
 };

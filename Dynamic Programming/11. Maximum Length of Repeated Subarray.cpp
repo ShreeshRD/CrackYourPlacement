d24c414839b5acd6edeb1ea40a59be145a7ef88d
ShreeshRD <93830912+ShreeshRD@users.noreply.github.com>
2024-09-09 12:51:19 +0530

    11

---
+++ Dynamic Programming/11. Maximum Length of Repeated Subarray.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findLength(vector<int> &nums1, vector<int> &nums2)
+    {
+        int m = nums1.size(), n = nums2.size(), max_len = 0;
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (nums1[i - 1] == nums2[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    if (dp[i][j] > max_len)
+                    {
+                        max_len = dp[i][j];
+                    }
+                }
+            }
+        }
+        return max_len;
+    }
+};
